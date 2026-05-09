b490c3b68d32380f0eb328be9cdc00d3aab791f7
Oleksandra Kozii <aleksandra200689@gmail.com>
2026-05-09 21:13:40 +0300

    restore validation logic

--- src/utils/validators.js
+++ src/utils/validators.js
@@ -1,7 +1,20 @@
-const validateBookingData = (data) => {
-  return true; 
+const validateBookingData = (data, sessionStartTime) => {
+  const errors = [];
+  const { seat_number } = data;
+
+  if (!seat_number || seat_number < 1 || seat_number > 100) {
+    errors.push('Seat number must be between 1 and 100');
+  }
+
+  if (sessionStartTime) {
+    const currentTime = new Date();
+    const sessionTime = new Date(sessionStartTime);
+    if (sessionTime <= currentTime) {
+      errors.push('Cannot book a ticket for a session that has already started or passed');
+    }
+  }
+
+  return errors;
 };
 
-module.exports = {
-  validateBookingData
-};
+module.exports = { validateBookingData };
